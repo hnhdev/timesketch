aeab1c0d692038f9b24b9b293f57bb8655d889ca
Zawadi Done <zawadi.done@huntandhackett.com>
2024-04-10 16:34:29 +0200

    Remove timestamp as mandatory field (#19)
    The analyzers and v1 of the frontend are out of scope, because we don't
    use these features.

--- timesketch/frontend-ng/src/components/Explore/EventList.vue
+++ timesketch/frontend-ng/src/components/Explore/EventList.vue
@@ -37,7 +37,7 @@ limitations under the License.
       <v-sheet class="d-flex flex-wrap mt-1 mb-5">
         <v-sheet class="flex-1-0">
           <span style="width: 200px" v-bind:style="getTimelineColor(highlightEvent)" class="datetime-table-cell pa-2">
-            {{ highlightEvent._source.timestamp | formatTimestamp | toISO8601 }}
+            {{ highlightEvent._source.datetime | formatTimestamp | toISO8601 }}
           </span>
         </v-sheet>
 
@@ -64,7 +64,7 @@ limitations under the License.
         must-sort
         :sort-desc.sync="sortOrderAsc"
         @update:sort-desc="sortEvents"
-        sort-by="_source.timestamp"
+        sort-by="_source.datetime"
         :hide-default-footer="totalHits < 11 || disablePagination"
         :expanded="expandedRows"
         :dense="displayOptions.isCompact"
@@ -337,9 +337,9 @@ limitations under the License.
         </template>
 
         <!-- Datetime field with action buttons -->
-        <template v-slot:item._source.timestamp="{ item }">
+        <template v-slot:item._source.datetime="{ item }">
           <div v-bind:style="getTimelineColor(item)" class="datetime-table-cell">
-            {{ item._source.timestamp | formatTimestamp | toISO8601 }}
+            {{ item._source.datetime | formatTimestamp | toISO8601 }}
           </div>
         </template>
 
@@ -597,7 +597,7 @@ export default {
         {
           text: 'Datetime (UTC) ',
           align: 'start',
-          value: '_source.timestamp',
+          value: '_source.datetime',
           width: '200',
           sortable: true,
         },
@@ -690,8 +690,8 @@ export default {
           return
         }
         let prevEvent = this.eventList.objects[index - 1]
-        let timestampMillis = this.$options.filters.formatTimestamp(event._source.timestamp)
-        let prevTimestampMillis = this.$options.filters.formatTimestamp(prevEvent._source.timestamp)
+        let timestampMillis = this.$options.filters.formatTimestamp(event._source.datetime)
+        let prevTimestampMillis = this.$options.filters.formatTimestamp(prevEvent._source.datetime)
         let timestamp = Math.floor(timestampMillis / 1000)
         let prevTimestamp = Math.floor(prevTimestampMillis / 1000)
         let delta = Math.floor(timestamp - prevTimestamp)
