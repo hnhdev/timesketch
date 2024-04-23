b411714b6a77bee0e354176d7777f1ffbde78f94
Zawadi Done <zawadi.done@huntandhackett.com>
2024-04-23 12:04:20 +0200

    Remove update by query

--- timesketch/api/v1/resources/event.py
+++ timesketch/api/v1/resources/event.py
@@ -1193,46 +1193,24 @@ class MarkEventsWithTimelineIdentifier(resources.ResourceMixin, Resource):
                 "sketch ID ({1:d})".format(sketch.id, timeline.sketch.id),
             )
 
-        # If a timeline is uploaded with a `timeline_filter_id` the adjusted
-        # query with the ID as filter is used.
-        timeline_filter_id = form.get("timeline_filter_id")
-        if timeline_filter_id is not None:
-            query_dsl = {
-                "script": {
-                    "source": (
-                        f"ctx._source.__ts_timeline_id={timeline_id};"
-                        f"ctx._source.timesketch_label=[];"
-                    ),
-                    "lang": "painless",
-                },
-                "query": {
-                    "bool": {
-                        "filter": {
-                            "term": {"__ts_timeline_filter_id": timeline_filter_id}
-                        },
-                        "must_not": {"exists": {"field": "__ts_timeline_id"}},
-                    }
-                },
-            }
-        else:
-            query_dsl = {
-                "script": {
-                    "source": (
-                        f"ctx._source.__ts_timeline_id={timeline_id};"
-                        f"ctx._source.timesketch_label=[];"
-                    ),
-                    "lang": "painless",
-                },
-                "query": {
-                    "bool": {
-                        "must_not": {
-                            "exists": {
-                                "field": "__ts_timeline_id",
-                            }
+        query_dsl = {
+            "script": {
+                "source": (
+                    f"ctx._source.__ts_timeline_id={timeline_id};"
+                    f"ctx._source.timesketch_label=[];"
+                ),
+                "lang": "painless",
+            },
+            "query": {
+                "bool": {
+                    "must_not": {
+                        "exists": {
+                            "field": "__ts_timeline_id",
                         }
                     }
-                },
-            }
+                }
+            },
+        }
         # pylint: disable=unexpected-keyword-arg
         self.datastore.client.update_by_query(
             body=query_dsl,
