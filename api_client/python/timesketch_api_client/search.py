e55ee58023de87867090eb19266cc41373dc84fa
Zawadi Done <zawadi.done@huntandhackett.com>
2025-02-13 15:27:39 +0100

    Test format

--- api_client/python/timesketch_api_client/search.py
+++ api_client/python/timesketch_api_client/search.py
@@ -1128,7 +1128,7 @@ class Search(resource.SketchResource):
         data_frame = pandas.DataFrame(return_list)
         try:
             data_frame["datetime"] = pandas.to_datetime(
-                data_frame.datetime, utc=True, format="ISO8601"
+                data_frame.datetime, utc=True, format="%Y-%m-%dT%H:%M:%S"
             )
         except pandas.errors.OutOfBoundsDatetime:
             pass
